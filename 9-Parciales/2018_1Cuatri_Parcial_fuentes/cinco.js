f07d9dd0a472238b85e6c530f6900398d36ac140
alumno <alumno>
2020-02-29 12:42:56 -0300

    parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,34 @@
 function mostrar()
-{
-
+{   
+    var planeta = prompt("Ingrese un planeta del sistema solar");
+    while(!isNaN(planeta))
+    {
+        prompt("ERROR, ingrese un nombre de un planeta");
+    
+    }
+    switch(planeta)
+    {
+        case "mercurio":
+        case "venus":
+        {
+            alert("acá hace calor");
+            break;
+        }
+        case "tierra":
+        {
+            alert("acá vivimos");
+            break;
+        }
+        case "marte":
+        case "jupiter":
+        case "saturno":
+        case "urano":
+        case "neptuno":
+        {
+            alert("acá hace más frío");
+            break;
+        }
+    
+    
+    }
 }
